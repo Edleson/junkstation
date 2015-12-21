47c80340f02aca484d5bd5c894294ce84fb930ba
Edleson Duarte <edleson.duarte@gmail.com>
2015-12-20 22:56:02 -0200

    PAGSEGURO

--- middlewares/pagseguroHandler.js
+++ middlewares/pagseguroHandler.js
@@ -19,31 +19,32 @@ module.exports = function(context) {
             	console.log(user.assinatura);
 
             	/*********************************************************
+			 	* Variáveis para serem utilizadas na assinatura          *
+			 	**********************************************************/
+        		var inicioVigiencia = new Date();
+        		var fimVigencia = utils.moment(inicioVigiencia).add(plano.expiracao, 'days');
+        		var status      = valorPlano == "0.00" ? "APROVADO"     : "AGUARDANDO PAGAMENTO";
+        		var operadora   = valorPlano == "0.00" ? "JUNKSTATION"  : "PAGSEGURO";    
+        		
+        		/*********************************************************
+			 	* Configura os dados da assinatura                       *
+			 	**********************************************************/
+        		var assinatura  = {
+        			user 				: id 				,
+        			plano 				: plano._id 		,
+        			nome_plano			: plano.titulo 		,
+        			valor_pago  		: valorPlano 		,
+        			operadora_cobranca  : operadora 		,
+        			inicio_vigencia     : inicioVigiencia	,
+        			fim_vigencia		: fimVigencia 		,
+        			status              : status				
+        		}
+
+            	/*********************************************************
 			 	* Caso o usuário não tenha assinatura entra no fluxo de  *
 			 	* inclusão de nova assinatura.                           *
 			 	**********************************************************/
             	if(user.assinatura === undefined){
-            		/*********************************************************
-				 	* Variáveis para serem utilizadas na assinatura          *
-				 	**********************************************************/
-            		var inicioVigiencia = new Date();
-            		var fimVigencia = utils.moment(inicioVigiencia).add(plano.expiracao, 'days');
-            		var status      = valorPlano == "0.00" ? "APROVADO"     : "AGUARDANDO PAGAMENTO";
-            		var operadora   = valorPlano == "0.00" ? "JUNKSTATION"  : "PAGSEGURO";    
-            		/*********************************************************
-				 	* Configura os dados da assinatura                       *
-				 	**********************************************************/
-            		var assinatura  = {
-            			user 				: id 				,
-            			plano 				: plano._id 		,
-            			nome_plano			: plano.titulo 		,
-            			valor_pago  		: valorPlano 		,
-            			operadora_cobranca  : operadora 		,
-            			inicio_vigencia     : inicioVigiencia	,
-            			fim_vigencia		: fimVigencia 		,
-            			status              : status				
-            		}
-
             		/*********************************************************
 				 	* Grava a assinatura na base de dados                    *
 				 	**********************************************************/
@@ -68,102 +69,13 @@ module.exports = function(context) {
 					 	* Atualiza os dados de assinatura do usuário logado      *
 					 	**********************************************************/
             			req.user.assinatura = newAss;
+            			/*********************************************************
+					 	* Inicia o fluxo de pagamento      *
+					 	**********************************************************/
+            			iniciaFluxoPagamento(req.user, plano, emailComprador, ddd, valorPlano, res);
             		});
-            	}
-
-            	if(user.assinatura.status === "COMPLETO" || user.assinatura.status === "APROVADO"){
-            		res.redirect("/anuncio/create");
             	}else{
-	            	/**********************************************************
-				 	* Verifica se é ambiente de testes                        *
-				 	**********************************************************/
-			    	pag = new pagseguro({
-				        email : context.cobranca.pagseguro.email ,
-				        token : context.cobranca.pagseguro.token 
-				        //mode  : 'sandbox'
-				    });
-			    	
-			    	/**********************************************************
-				 	* Verifica se é ambiente de testes                        *
-				 	**********************************************************/
-				    if(context.cobranca.pagseguro.ambiente === "sandbox"){
-				    	pag.mode       = "sandbox";
-				    	emailComprador = context.cobranca.pagseguro.comprador; 
-				    }
-				    
-				    /*********************************************************
-				 	* Seta a sigla da moeda corrente                         *
-				 	**********************************************************/
-				    pag.currency('BRL');
-				    
-				    /*********************************************************
-				 	* Seta o código de referencia da compra                  *
-				 	**********************************************************/
-				    pag.reference(plano._id);
-				    
-				    /*********************************************************
-				 	* Add o item que será utilizado para compra              *
-				 	**********************************************************/
-				    pag.addItem({
-				        id 			: 1,
-				        description : "Assintura do plano " + plano.titulo + " na Junk Station",
-				        amount 		: valorPlano ,
-				        quantity 	: 1
-				    });
-				    
-				    /*********************************************************
-				 	* Configura as informações do comprador                  *
-				 	**********************************************************/
-				    pag.buyer({
-				        name          : user.dadosPessoais.nome	,
-				        email         : emailComprador			,
-				        phoneAreaCode : ddd 					,
-				        phoneNumber   : numero
-				    });
-				    
-				    /*********************************************************
-				 	* Configurando as informações de entrega                 *
-				 	**********************************************************/
-				    pag.shipping({
-				        type 		: 1										,
-				        street 		: user.dadosPessoais.logradouro			,
-				        number 		: user.dadosPessoais.numeroLogradouro 	,
-				        complement 	: user.dadosPessoais.complemento		,
-				        district 	: user.dadosPessoais.bairro				,
-				        postalCode 	: user.dadosPessoais.cep				,
-				        city 		: user.dadosPessoais.cidade				,
-				        state 		: user.dadosPessoais.estado				,
-				        country 	: 'BRA'
-				    });
-				    
-				    /*********************************************************
-				 	* Seta as urls de notificação e redirecionamento         *
-				 	**********************************************************/
-				 	pag.setRedirectURL(context.cobranca.pagseguro.getRedirectUrl());
-				    pag.setNotificationURL(context.cobranca.pagseguro.getNotificationUrl());
-				    
-				    /*********************************************************
-				 	* Envia a solicitação para o pagseguro                   *
-				 	**********************************************************/
-				   	pag.send(function(err, response) {
-				   		console.log(response);
-				        if (err) {
-				            next(err);
-				        }else{
-				        	xml2Json.parseString(response, function(error, result){
-					        	if(error){
-					        		next(error);
-					        	}else{
-					        		if(result.errors){
-
-					        		}else{
-					        			var checkoutUrl = context.cobranca.pagseguro.buildUrlCheckout(result.checkout.code[0]);
-					        			res.redirect(checkoutUrl);
-					        		}
-					        	}
-							});
-				        } 
-				    });	
+            		res.redirect("/anuncio/create");
             	}
             }
         });
@@ -184,6 +96,103 @@ module.exports = function(context) {
 		}else{
 			return false;
 		}
+	}
+
+	function iniciaFluxoPagamento(user, plano, emailComprador, ddd, valorPlano, res  ){
+		if(user.assinatura.status === "COMPLETO" || user.assinatura.status === "APROVADO"){
+    		res.redirect("/anuncio/create");
+    	}else{
+        	/**********************************************************
+		 	* Verifica se é ambiente de testes                        *
+		 	**********************************************************/
+	    	pag = new pagseguro({
+		        email : context.cobranca.pagseguro.email ,
+		        token : context.cobranca.pagseguro.token 
+		        //mode  : 'sandbox'
+		    });
+	    	
+	    	/**********************************************************
+		 	* Verifica se é ambiente de testes                        *
+		 	**********************************************************/
+		    if(context.cobranca.pagseguro.ambiente === "sandbox"){
+		    	pag.mode       = "sandbox";
+		    	emailComprador = context.cobranca.pagseguro.comprador; 
+		    }
+		    
+		    /*********************************************************
+		 	* Seta a sigla da moeda corrente                         *
+		 	**********************************************************/
+		    pag.currency('BRL');
+		    
+		    /*********************************************************
+		 	* Seta o código de referencia da compra                  *
+		 	**********************************************************/
+		    pag.reference(plano._id);
+		    
+		    /*********************************************************
+		 	* Add o item que será utilizado para compra              *
+		 	**********************************************************/
+		    pag.addItem({
+		        id 			: 1,
+		        description : "Assintura do plano " + plano.titulo + " na Junk Station",
+		        amount 		: valorPlano ,
+		        quantity 	: 1
+		    });
+		    
+		    /*********************************************************
+		 	* Configura as informações do comprador                  *
+		 	**********************************************************/
+		    pag.buyer({
+		        name          : user.dadosPessoais.nome	,
+		        email         : emailComprador			,
+		        phoneAreaCode : ddd 					,
+		        phoneNumber   : numero
+		    });
+		    
+		    /*********************************************************
+		 	* Configurando as informações de entrega                 *
+		 	**********************************************************/
+		    pag.shipping({
+		        type 		: 1										,
+		        street 		: user.dadosPessoais.logradouro			,
+		        number 		: user.dadosPessoais.numeroLogradouro 	,
+		        complement 	: user.dadosPessoais.complemento		,
+		        district 	: user.dadosPessoais.bairro				,
+		        postalCode 	: user.dadosPessoais.cep				,
+		        city 		: user.dadosPessoais.cidade				,
+		        state 		: user.dadosPessoais.estado				,
+		        country 	: 'BRA'
+		    });
+		    
+		    /*********************************************************
+		 	* Seta as urls de notificação e redirecionamento         *
+		 	**********************************************************/
+		 	pag.setRedirectURL(context.cobranca.pagseguro.getRedirectUrl());
+		    pag.setNotificationURL(context.cobranca.pagseguro.getNotificationUrl());
+		    
+		    /*********************************************************
+		 	* Envia a solicitação para o pagseguro                   *
+		 	**********************************************************/
+		   	pag.send(function(err, response) {
+		   		console.log(response);
+		        if (err) {
+		            next(err);
+		        }else{
+		        	xml2Json.parseString(response, function(error, result){
+			        	if(error){
+			        		next(error);
+			        	}else{
+			        		if(result.errors){
+
+			        		}else{
+			        			var checkoutUrl = context.cobranca.pagseguro.buildUrlCheckout(result.checkout.code[0]);
+			        			res.redirect(checkoutUrl);
+			        		}
+			        	}
+					});
+		        } 
+		    });	
+    	}	
 	}
     		
     return pagseguroHandler;
