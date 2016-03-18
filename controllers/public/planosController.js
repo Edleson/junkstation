fa6003a1ffc9c5c32b6ab9af0d603ace70954c3e
Edleson Duarte <edleson.duarte@gmail.com>
2016-03-18 18:01:53 -0300

    Planos

--- controllers/public/planosController.js
+++ controllers/public/planosController.js
@@ -4,12 +4,21 @@ module.exports = function(app) {
     var responseObject = {};
 
     controller.index = function(req, res, next) {
-        var query = {situacao : true, tipoPessoa : "PF"};
+        var query = {situacao : true};
         Plano.findByQuery(function(err, planos){
             if(err){
                 console.log(err);
                 throw Exception("Ocorreu um erro durante o processamento da requisição ! :(");
             }else{
+                if(req.user && req.user.dadosPessoais.tipopessoa == "PJ"){
+                    planos  = planos.filter(function(item){
+                        return item.tipoPessoa == 'PJ';
+                    });
+                }else{
+                    planos  = planos.filter(function(item){
+                        return item.tipoPessoa == 'PF';
+                    });
+                }
                 responseObject.planos = planos;
                 res.render("planos", {response : responseObject});
             }
